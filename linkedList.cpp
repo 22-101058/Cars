dcc07eb393cfb6d95a1d2dca0841e78c1f22c8ae
22-101058 <22-101058@students.eui.edu.eg>
2023-12-21 23:51:33 +0200

    linkedList files added

---
+++ linkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amish on 12/21/2023.
+//
+
+#include "linkedList.h"
---
+++ linkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by amish on 12/21/2023.
+//
+
+#ifndef CARS_LINKEDLIST_H
+#define CARS_LINKEDLIST_H
+
+
+class linkedList {
+
+};
+
+
+#endif //CARS_LINKEDLIST_H
